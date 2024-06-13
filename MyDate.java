fbcaf22bc2a35b8f86830197b37f4c66f791a920
ahask93 <ahask93@gmail.com>
2024-06-13 09:45:31 -0400

    basic code structure

---
+++ MyDate.java
@@ -0,0 +1,25 @@
+public class MyDate {
+    int year = 1970;
+    int month = 0;
+    int day = 1;
+
+    MyDate () {
+    }
+
+    int getYear () {
+        return year;
+    }
+    
+    int getMonth() {
+        return month;
+    }
+
+    int getDay(){
+        return day;
+    }
+
+    void setDate(long elapsedTime){
+        
+    }
+}
+
