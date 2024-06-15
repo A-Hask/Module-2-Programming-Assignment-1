5ca33408aade7986d4058892bc84ed4b4e6ed424
ahask93 <ahask93@gmail.com>
2024-06-15 15:06:27 -0400

    fully working code

--- MyDate.java
+++ MyDate.java
@@ -1,14 +1,32 @@
 import java.util.GregorianCalendar;
 
 public class MyDate {
-    int year = 1970;
-    int month = 0;
-    int day = 1;
+    private int year = 0;
+    private int month = 0;
+    private int day = 0;
 
     MyDate () {
+        GregorianCalendar gregorianCalendar = new GregorianCalendar();
+        this.year = gregorianCalendar.get(gregorianCalendar.YEAR);
+        this.month = gregorianCalendar.get(gregorianCalendar.MONTH);
+        this.day = gregorianCalendar.get(gregorianCalendar.DAY_OF_MONTH);
     }
 
-    int getYear () {
+    MyDate(long elapsedTime){
+        setDate(elapsedTime);
+    }
+
+
+    void setDate(long elapsedTime){
+        GregorianCalendar gregorianCalendar = new GregorianCalendar();
+        gregorianCalendar.setTimeInMillis(elapsedTime);
+        this.year = gregorianCalendar.get(gregorianCalendar.YEAR);
+        this.month = gregorianCalendar.get(gregorianCalendar.MONTH);
+        this.day = gregorianCalendar.get(gregorianCalendar.DAY_OF_MONTH);
+        System.out.println("The year is " + year + ", The month is " + ( month + 1) + ", and the day is " + day + ".");
+    }
+
+    int getYear() {
         return year;
     }
     
@@ -20,8 +38,13 @@ public class MyDate {
         return day;
     }
 
-    void setDate(long elapsedTime){
-        
+    public static void main(String[] args){
+        MyDate date1 = new MyDate();
+        date1.setDate(0L);
+        MyDate date2 = new MyDate();
+        date2.setDate(34355555133101L);
+
     }
+
 }
 
